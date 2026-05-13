76cda6f4bdf0686a48d74b0017e8427fe6f33819
virgula <angelo.rosa@protonmail.com>
2026-05-13 17:28:10 +0200

    template

---
+++ nmap/nmap.py
@@ -0,0 +1,83 @@
+from src.abstract import Module
+from src.docker_custom import CustomClient
+from pathlib import Path
+import os
+
+
+class Nmap(Module):
+    _url = ""
+    _target_name = "nmap"
+    _lts_file = os.path.join(Path(__file__).resolve().parent, "lts_version")
+    _stable_file = os.path.join(
+        Path(__file__).resolve().parent, "stable_working_version"
+    )
+    _compiled_file = os.path.join(Path(__file__).resolve().parent, "compiled_versions")
+    _compose_file = os.path.join(Path(__file__).resolve().parent, "docker-compose.yaml")
+
+    _verbose = False
+    _docker_client = None
+
+    _current_version = "0"
+    _docker_env_version = "0"
+    _stable_fallback_version = "0"
+
+    def __init__(self, verbose, docker_client: CustomClient):
+        super().__init__(verbose=verbose)
+        self._verbose = verbose
+        self._docker_client = docker_client
+
+        self._current_version = self.read_version_from_file(self._lts_file)
+        self._docker_env_version = f"NMAP_VERSION={self._current_version}"
+
+        self._stable_fallback_version = self.read_version_from_file(self._stable_file)
+
+    def __str__(self):
+        return self._target_name.upper()
+
+    def update_version(self):
+        try:
+            pass
+        except Exception as ex:
+            print(ex)
+            return False
+
+        return True
+
+    def _build(self, service):
+        result = self._docker_client.run(
+            service=service,
+            compose_file=self._compose_file,
+            version=self._docker_env_version,
+        )
+
+        if result:
+            self.append_compiled(service=service, version=self._current_version)
+            return result
+
+        self.logger(
+            f"ERROR for {service}! compilation not successful for version {self._current_version} "
+            f"falling back and compile {self._stable_fallback_version}"
+        )
+
+        result = self._docker_client.run(
+            service=service,
+            compose_file=self._compose_file,
+            version=f"NMAP_VERSION={self._stable_fallback_version}",
+        )
+
+        if result:
+            self.append_compiled(service=service, version=self._stable_fallback_version)
+
+        return result
+
+    def build_x86_64(self):
+        return
+
+    def build_x86(self):
+        return
+
+    def build_arm64(self):
+        return
+
+    def build_arm32(self):
+        return
